be4ed15d74aeb66d73ec89d25667341e00e176b3
kareem <49622102+KnowledgeBI@users.noreply.github.com>
2021-01-12 22:16:38 +0200

    destenation account app

---
+++ destination_account/models/account_payment.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, exceptions, _
+from datetime import date, datetime, time, timedelta
+from odoo.fields import Date, Datetime
+from odoo.tools import float_compare
+import odoo.addons.decimal_precision as dp
+from odoo.exceptions import UserError, ValidationError
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    destination_account_id = fields.Many2one('account.account', compute=False, readonly=False)
+
+    @api.onchange('invoice_ids', 'payment_type', 'partner_type', 'partner_id')
+    def _onchange_destination_account_id(self):
+        if self.invoice_ids:
+            self.destination_account_id = self.invoice_ids[0].account_id.id
+        elif self.payment_type == 'transfer':
+            if not self.company_id.transfer_account_id.id:
+                raise UserError(_(
+                    'There is no Transfer Account defined in the accounting settings. Please define one to be able to confirm this transfer.'))
+            self.destination_account_id = self.company_id.transfer_account_id.id
+        elif self.partner_id:
+            if self.partner_type == 'customer':
+                self.destination_account_id = self.partner_id.property_account_receivable_id.id
+            else:
+                self.destination_account_id = self.partner_id.property_account_payable_id.id
+        elif self.partner_type == 'customer':
+            default_account = self.env['ir.property'].get('property_account_receivable_id', 'res.partner')
+            if default_account:
+                self.destination_account_id = default_account.id
+        elif self.partner_type == 'supplier':
+            default_account = self.env['ir.property'].get('property_account_payable_id', 'res.partner')
+            if default_account:
+                self.destination_account_id = default_account.id
